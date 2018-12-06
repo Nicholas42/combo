ff3cc8938a0618559fb30103be578d7020c4bc5f
Nicholas <nics-lohr@gmx.de>
2018-12-07 00:07:45 +0100

    Added class for t-join. Should blackbox everything safe these two methods.

---
+++ prog2/tjoin.hpp
@@ -0,0 +1,17 @@
+#ifndef PROG2_TJOIN_HPP
+#define PROG2_TJOIN_HPP
+
+#include "graph.hpp"
+
+namespace MIN_MEAN_WEIGHT_CYCLE
+{
+class TJoin
+{
+  public:
+    capacity get_capacity() const;
+
+    EdgeId get_num_edges() const;
+};
+}   // namespace MIN_MEAN_WEIGHT_CYCLE
+
+#endif   // PROG2_TJOIN_HPP
